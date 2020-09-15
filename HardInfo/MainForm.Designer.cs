8412bb7ebb8e1adcf683ec10e7dac8d811fee143
Dmitry Zaitsev <dmitry.zaitsev.1993@gmail.com>
2020-09-15 12:16:29 +0200

    Adding project to repository.

---
+++ HardInfo/MainForm.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace HardInfo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(900, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 28);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(900, 28);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Items.AddRange(new object[] {
+            "Processor",
+            "Graphics Card",
+            "Chipset",
+            "Battery",
+            "BIOS",
+            "RAM",
+            "Cache",
+            "USB",
+            "Disk",
+            "Logical Disks",
+            "Keyboard",
+            "Network",
+            "Users"});
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 28);
+            this.toolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox1_SelectedIndexChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnName,
+            this.columnValue});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 56);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(900, 591);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnName
+            // 
+            this.columnName.Text = "Name";
+            this.columnName.Width = 200;
+            // 
+            // columnValue
+            // 
+            this.columnValue.Text = "Value";
+            this.columnValue.Width = 200;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 647);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainForm";
+            this.Text = "Hardware Info";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnName;
+        private System.Windows.Forms.ColumnHeader columnValue;
+    }
+}
+
---
+++ HardInfo/MainForm.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Management;
+using System.Drawing;
+
+namespace HardInfo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GetHardwareInfo(string key, ListView list)
+        {
+            list.Items.Clear();
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM " + key);
+
+            try
+            {
+                foreach(ManagementObject obj in searcher.Get())
+                {
+                    ListViewGroup listViewGroup;
+
+                    try
+                    {
+                        listViewGroup = list.Groups.Add(obj["Name"].ToString(), obj["Name"].ToString());
+                    }
+                    catch
+                    {
+                        listViewGroup = list.Groups.Add(obj.ToString(), obj.ToString());
+                    }
+
+                    if(obj.Properties.Count == 0)
+                    {
+                        MessageBox.Show("Could not get information", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    foreach(PropertyData data in obj.Properties)
+                    {
+                        ListViewItem item = new ListViewItem(listViewGroup);
+
+                        if (list.Items.Count % 2 != 0) item.BackColor = Color.White;
+                        else item.BackColor = Color.WhiteSmoke;
+
+                        item.Text = data.Name;
+
+                        if(data.Value != null && !string.IsNullOrEmpty(data.Value.ToString()))
+                        {
+                            switch(data.Value.GetType().ToString())
+                            {
+                                case "System.String[]":
+
+                                    string[] stringData = data.Value as string[];
+                                    string resStr1 = string.Empty;
+
+                                    foreach(string s in stringData)
+                                    {
+                                        resStr1 += $"{s} ";
+                                    }
+                                    item.SubItems.Add(resStr1);
+
+                                    break;
+
+                                case "System.UInt16[]":
+
+                                    ushort[] ushortData = data.Value as ushort[];
+                                    string resStr2 = string.Empty;
+
+                                    foreach(ushort u in ushortData)
+                                    {
+                                        resStr2 += $"{Convert.ToString(u)} ";
+                                    }
+
+                                    item.SubItems.Add(resStr2);
+
+                                    break;
+
+                                default:
+
+                                    item.SubItems.Add(data.Value.ToString());
+
+                                    break;
+                            }
+
+                            list.Items.Add(item);
+                        }
+                    }
+                }
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string key = string.Empty;
+
+            switch(toolStripComboBox1.SelectedItem.ToString())
+            {
+                case "Processor":
+                    key = "Win32_Processor";
+                    break;
+                case "Graphics Card":
+                    key = "Win32_VideoController";
+                    break;
+                case "Chipset":
+                    key = "Win32_IDEController";
+                    break;
+                case "Battery":
+                    key = "Win32_Battery";
+                    break;
+                case "BIOS":
+                    key = "Win32_BIOS";
+                    break;
+                case "RAM":
+                    key = "Win32_PhysicalMemory";
+                    break;
+                case "Cache":
+                    key = "Win32_CacheMemory";
+                    break;
+                case "USB":
+                    key = "Win32_USBController";
+                    break;
+                case "Disk":
+                    key = "Win32_DiskDrive";
+                    break;
+                case "Logical Disks":
+                    key = "Win32_LogicalDisk";
+                    break;
+                case "Keyboard":
+                    key = "Win32_Keyboard";
+                    break;
+                case "Network":
+                    key = "Win32_NetworkAdapter";
+                    break;
+                case "Users":
+                    key = "Win32_Account";
+                    break;
+                default:
+                    key = "Win32_Processor";
+                    break;
+            }
+
+            GetHardwareInfo(key, listView1);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            ShowIcon = false;
+            toolStripComboBox1.SelectedIndex = 0;
+        }
+    }
+}
